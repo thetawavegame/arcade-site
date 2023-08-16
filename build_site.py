7ce26549e1007d060b365e9dfc562c8c3c08844e
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-08-16 17:06:26 -0500

    Add a build Python build script for the site to copy a file or two. Minimal stuff.

---
+++ build_site.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Trivial vanilla-python script to copy a file to a directory"""
+import enum
+import argparse
+from pathlib import Path
+from typing import Optional, Sequence
+import shutil
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+@enum.unique
+class AppTier(str, enum.Enum):
+    DEV = "dev"
+    PROD = "prod"
+
+
+class CLIArgs(argparse.Namespace):
+    tier: AppTier
+
+
+def our_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="build-arcade-site",
+        description="Build the arcade.metalmancy.tech website to be served by a CDN/routing thing.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "tier",
+        help="The application environment. dev or prod.",
+        choices=[x.value for x in AppTier.__members__.values()],
+        type=AppTier,
+    )
+    return parser
+
+
+def run(args: CLIArgs) -> None:
+    base_dir = Path(__file__).parent
+    config_fname = f"netlify_{args.tier.value}.toml"
+    config_file = base_dir / config_fname
+    dest_config_path = base_dir / "public" / "netlify.toml"
+    if not config_file.exists():  # pragma: no cover
+        raise FileNotFoundError(
+            f"Make sure that the netlify files exist. {base_dir=} {config_file=}"
+        )
+    shutil.copy(config_file, dest_config_path)
+    LOGGER.info("Copied %s to %s", config_file, dest_config_path)
+
+
+def main(args: Optional[Sequence[str]] = None):
+    parser = our_parser()
+    opts: CLIArgs = parser.parse_args(args, CLIArgs())
+    run(opts)
+
+
+if __name__ == "__main__":
+    main()
